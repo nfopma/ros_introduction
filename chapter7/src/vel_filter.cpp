3cb1f5dc235c7f9b4fd52b3c4dd430a377098536
nfopma <nathancfopma@gmail.com>
2018-05-30 12:11:24 +0200

    Begonnen met tf2 tutorial

--- chapter7/src/vel_filter.cpp
+++ chapter7/src/vel_filter.cpp
@@ -1,5 +1,6 @@
 # include <ros/ros.h>
 # include <geometry_msgs/Twist.h>
+# include <std_srvs/Empty.h>
 
 
 class VelocityFilter
@@ -7,27 +8,43 @@ class VelocityFilter
 public:
   VelocityFilter(ros::NodeHandle& nh)
     : velocity_sub_(nh.subscribe("turtle1/cmd_vel", 10, &VelocityFilter::filter, this)),
-      velocity_pub_(nh.advertise<geometry_msgs::Twist>("filtered_velocity", 10))
+      velocity_pub_(nh.advertise<geometry_msgs::Twist>("filtered_velocity", 10)),
+      filter_switch_serv_(nh.advertiseService("filter_switch", &VelocityFilter::filter_switch, this)),
+      filter_switched_on_("true")
   {
-    max_ang_vel_loaded = ros::param::get("~max_vel", max_ang_vel);
+    max_ang_vel_loaded_ = ros::param::get("~max_vel", max_ang_vel_);
   }
 
   void filter(const geometry_msgs::Twist& velocity_cmd)
   {
-    if(!max_ang_vel_loaded)
+    if(filter_switched_on_)
     {
-      ROS_FATAL_STREAM("Maximum Angular Velocity NOT LOADED");
-    } else if(velocity_cmd.angular.z < max_ang_vel)
+      if(!max_ang_vel_loaded_)
+      {
+        ROS_FATAL_STREAM("Maximum Angular Velocity NOT LOADED");
+      } else if(velocity_cmd.angular.z < max_ang_vel_)
+      {
+        velocity_pub_.publish(velocity_cmd);
+      }
+    } else
     {
       velocity_pub_.publish(velocity_cmd);
     }
   }
 
+  bool filter_switch(std_srvs::Empty::Request &req, std_srvs::Empty::Response &resp)
+  {
+    filter_switched_on_ = !filter_switched_on_;
+  }
+
 private:
   ros::Subscriber velocity_sub_;
   ros::Publisher velocity_pub_;
-  double max_ang_vel;
-  bool max_ang_vel_loaded;
+  ros::ServiceServer filter_switch_serv_;
+
+  bool filter_switched_on_;
+  double max_ang_vel_;
+  bool max_ang_vel_loaded_;
 };
 
 int main(int argc, char** argv)
